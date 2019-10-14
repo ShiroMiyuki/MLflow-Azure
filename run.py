5b329821da33395d0c1e587c3e001bad4b4ead94
ShiroMiyuki <38790881+ShiroMiyuki@users.noreply.github.com>
2019-10-14 13:00:46 +0200

    tracking model with mlflow, azure yaml, azure deployment script, run script

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+from subprocess import Popen
+import webbrowser
+import time
+
+mlflow_port = 5002
+
+Popen(f"cmd /c mlflow ui --port={mlflow_port}", shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+
+# give a sec till this start-up
+time.sleep(1)
+
+webbrowser.open(f"http://kubernetes.docker.internal:{mlflow_port}")
+
+Popen("cmd /c jupyter notebook --notebook-dir=notebooks", shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
